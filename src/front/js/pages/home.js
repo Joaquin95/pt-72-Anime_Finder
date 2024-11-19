f3a82c92fe57d77b2c5c7e2c3a969216943e645d
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-11-19 00:50:17 +0000

    set up signup page

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,17 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
-
+	console.log(store.token)
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
+			<Link to={"/signup"} className="btn btn-primary m-2">Signup</Link>
+			<Link to={"/login"} className="btn btn-info m-2">Login</Link>
+			<Link to={"/profile"} className="btn btn-success m-2">Profile</Link>
 		</div>
 	);
 };
---
+++ src/front/js/pages/signup.js
@@ -0,0 +1,36 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+
+export const Signup = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPasword] = useState("");
+	const { store, actions } = useContext(Context);
+    const navigate = useNavigate()
+
+    const createUser = async() => {
+        let response = await fetch(process.env.BACKEND_URL + "/signup", {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify({ 
+                email: email,
+                password: password
+            })
+        })
+        let data = await response.json()
+        navigate("/")
+    }
+
+	return (
+		<div className="text-center mt-5">
+			<div className="input-group mb-3">
+                <span className="input-group-text" id="inputGroup-sizing-default">Email</span>
+                <input type="text" onChange={(e) => setEmail(e.target.value)} className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+            </div>
+            <div className="input-group mb-3">
+                <span className="input-group-text" id="inputGroup-sizing-default">Password</span>
+                <input type="password"  onChange={(e) => setPasword(e.target.value)}  className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+            </div>
+            <button className="btn btn-info" onClick={() => createUser()}>Sign up</button>
+		</div>
+	);
+};
