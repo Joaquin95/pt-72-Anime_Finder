bba2dff037528e1c2124c3749f4495f8c316687b
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-11-22 23:42:47 +0000

    CSS fixing

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,24 +3,24 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar bs-info-bg-subtle text-info">
+		<nav className="navbar bg-primary bs-info-bg-subtle text-info">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">Anime Finder</span>
+					<span className="navbar-brand text-dark mb-0 h1">Anime Finder</span>
 				</Link>
 				<div className="ml-auto nav-item dropdown">
-					<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+					<a className="nav-link dropdown-toggle text-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
 						List
 					</a>
-					<div className="dropdown-menu h1 m-auto">
+					<div className="dropdown-menu bg-dark h1 m-auto">
 						<Link to="/signup">
-							<button className="btn btn-primary m-1">Sign Up</button>
+							<button className="btn btn-primary text-dark m-1">Sign Up</button>
 						</Link>
 						<Link to="/login">
-							<button className="btn btn-primary m-1">Login</button>
+							<button className="btn btn-primary text-dark m-1">Login</button>
 						</Link>
 						<Link to="/profile">
-							<button className="btn btn-primary m-1">Profile</button>
+							<button className="btn btn-primary text-dark m-1">Profile</button>
 						</Link>
 					</div>
 				</div>
