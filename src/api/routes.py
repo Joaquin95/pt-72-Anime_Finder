fe062cdf8c89d80f8a15f39d008c64d79a4c2234
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-11-25 23:52:56 +0000

    user favorites added

--- src/api/routes.py
+++ src/api/routes.py
@@ -53,3 +53,66 @@ def get_user():
     user = User.query.filter_by(email=email).first()
     return jsonify(user.serialize())
 
+@app.route('/favoritePeople', methods=['POST'])
+def create_favpeople():
+    request_fav_people = request.get_json()
+
+    new_fav_people = FavoritePeople(user_id=request_fav_people["user"], people_id=request_fav_people["people"])
+    db.session.add(new_fav_people)
+    db.session.commit()
+
+    return jsonify(request_fav_people), 200 
+
+
+@app.route('/favoritePeople', methods=['GET'])
+def get_favpeople():
+
+    fav_people = FavoritePeople.query.all()
+    all_favpeople = list(map(lambda x: x.serialize(), fav_people))
+    return jsonify(all_favpeople), 200 
+
+
+@app.route('/<int:user_id>/favoritePeople', methods=['GET'])
+def get_user_favpeople(user_id):
+
+    userfav_people = FavoritePeople.query.filter_by(user_id=user_id).all()
+
+    if not userfav_people:
+        return {"message": "Este user no ha marcado ningún personaje favorito"}, 404
+   
+    serialized_favorite_people = [{
+        "ID": favorite.people_id,
+        "Character's name": People.query.get(favorite.people_id).name
+    } for favorite in userfav_people]
+
+    return {'Personajes favoritos del usuario': serialized_favorite_people}, 200
+
+@app.route('/user/<int:user_id>/people/<int:people_id>', methods=['POST'])
+def post_user_favpeople(user_id, people_id):
+
+    user = User.query.get(user_id)
+    if not user:
+        return {"error": "El usuario no existe"}, 404
+    
+    people = People.query.get(people_id)
+    if not people:
+        return {"error": "El personaje no existe"}, 404
+    
+    new_fav_people = FavoritePeople(user_id=user_id, people_id=people_id)
+    db.session.add(new_fav_people)
+    db.session.commit()
+    
+    return {"message": "Personaje agregado como favorito existosamente"}
+
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_favpeople(people_id):
+
+    favpeople = FavoritePeople.query.filter_by(people_id=people_id).all()
+    if not favpeople:
+        return {"Error": "No hay registros de favoritos de este personaje"}
+    
+    for person in favpeople:
+        db.session.delete(person)
+    db.session.commit()
+
+    return {"message": "Registros de este personaje eliminados de favoritos"}, 200
