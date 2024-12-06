a4ea7c5989dff09f7ce822f5d0ee8e83e30731bf
amelv-03 <aiden.melv03@gmail.com>
2024-12-06 23:15:42 +0000

    started streaming function

--- src/front/js/pages/searchAnime.js
+++ src/front/js/pages/searchAnime.js
@@ -1,9 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 export const SearchAnime = () => {
 	const { store, actions } = useContext(Context);
+    const [anime, setStreaming] = useState({})
+    const { id } = useParams();
+
+	useEffect(() => {
+        async function searchAnime() {
+            const response = await fetch("https://api.jikan.moe/v4/anime/{id}/streaming" + id + "/full")
+            const data = await response.json()
+            setStreaming(data.data) 
+        }
+        searchAnime() 
+    }, [])	
 	return (
 		<div className="text-center mt-5 bg-dark">
 			
