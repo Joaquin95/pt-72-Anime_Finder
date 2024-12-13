29e50f77eafdacdf21fc8477d478c1d7c84c1343
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-12-13 23:53:54 +0000

    resloving merge changes

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -12,8 +12,40 @@ export const Home = () => {
 	
 	
 	return (
-		<div className="text-center mt-5 bg-dark">
-			
+		<div className="main-div w-100 bg-dark">
+			<div className="d-flex flex-column w-100 mt-0 align-item-center">
+				{/* Anime card div */}
+				<h1 className="m-3">Popular Anime</h1> 
+				<div id="cardDiv" className="d-flex flex-nowrap overflow-scroll align-items-stretch">
+					{store.anime.map((item, index) => {
+						return (
+							<AnimeCard item={item} index={index} key={index} category="anime" /> 
+						)
+					})}
+				</div>
+			</div>
+			<div className="d-flex flex-column w-100 mt-0 align-item-center">
+				{/* Anime card div */}
+				<h1 className="m-2">Classic Anime</h1>
+				<div id="cardDiv" className="d-flex flex-nowrap overflow-scroll align-items-stretch">
+					{store.anime.map((item, index) => {
+						return (
+							<AnimeCard item={item} index={index} key={index} category="anime" />
+						)
+					})}
+				</div>
+			</div>
+			<div className="d-flex flex-column w-100 mt-0 align-item-center">
+				{/* Anime card div */}
+				<h1 className="m-2">Popular Manga</h1>
+				<div id="cardDiv" className="d-flex flex-nowrap overflow-scroll align-items-stretch">
+					{store.manga.map((item, index) => {
+						return (
+							<MangaCard item={item} index={index} key={index} category="anime" /> 
+						)
+					})}
+				</div>
+			</div>
 		</div>
 	);
 };
